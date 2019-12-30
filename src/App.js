9206dde880a66d30c4c8a6a11029e903365527eb
Tommy Vinyard <tpvinyard@gmail.com>
2019-12-29 22:56:59 -0600

    cleaned up files and add checkGuess function to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    //characters: characters,
+    pickedCharacters: [],
+    topScore: 0
+  }
+
+  checkGuess = (name) => {
+    const newState = { ...this.state };
+    if (newState.pickedCharacters.includes(name)) {
+      newState.pickedCharacters = []
+      alert("you lose");
+    } else {
+      newState.pickedCharacters.push(name);
+      this.setState({ newState })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Made it!</h1>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
