ad2c7b92106fd0b29d44b8ef19146e2a814bca4e
Tommy Vinyard <tpvinyard@gmail.com>
2020-01-07 16:01:26 -0600

    Updates

---
+++ precache-manifest.aa34f86dd2f3fb05e2f41f6fac8e1d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95340feff3475710accc1d001dc6a230",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "343666fd29da3be96154",
+    "url": "/clicky-game/static/css/main.7470748f.chunk.css"
+  },
+  {
+    "revision": "1d72f8cd827e95d8fb11",
+    "url": "/clicky-game/static/js/2.29d0578e.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/clicky-game/static/js/2.29d0578e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "343666fd29da3be96154",
+    "url": "/clicky-game/static/js/main.2d079b82.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
